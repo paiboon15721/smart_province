dedf44cfa0e53953db64f6b0719456e5406267f1
Paiboon Auengkongkatong <paiboon15721@gmail.com>
2014-05-22 14:42:36 +0700

    fix

--- app/controllers/ExternalController.php
+++ app/controllers/ExternalController.php
@@ -41,7 +41,7 @@ class ExternalController extends BaseController {
     }
 
     public function login() {
-        echo base_path();
+        echo base_url();
         exit;
         return Redirect::to("signin/signin.application?ACT_FLAG='" . URL::base() . "/write_session'&ACT_FLAG_CANCEL=");
     }
