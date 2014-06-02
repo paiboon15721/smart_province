2967aab68b2557dbc4d5925aae5cc83c7d4def50
Paiboon Auengkongkatong <paiboon15721@gmail.com>
2014-06-02 18:45:22 +0700

    fix design

--- app/views/content/contactUs.blade.php
+++ app/views/content/contactUs.blade.php
@@ -1,15 +1,15 @@
 @extends('contentLayouts.index')
 
 @section('subTitle')
-	ติดต่อเรา
+ติดต่อเรา
 @stop
 @section('subDescription')
-	ติดต่อเรา
+ติดต่อเรา
 @stop
 @section('subKeywords')
-	ติดต่อเรา
+ติดต่อเรา
 @stop
 
 @section('subContent')
-	<img src="{{asset('images/main/contactUs.jpg')}}"/>
+<img src="{{asset('images/main/contactUs.jpg')}}"/>
 @stop
