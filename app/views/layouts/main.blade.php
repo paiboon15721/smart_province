286427827d2a835f90d27c5a54bca0893fdb8c7d
Paiboon Auengkongkatong <paiboon15721@gmail.com>
2014-06-03 10:33:52 +0700

    fix new design

--- app/views/layouts/main.blade.php
+++ app/views/layouts/main.blade.php
@@ -111,8 +111,10 @@ header("Expires: Sat, 26 Jul 1997 05:00:00 GMT");
                 </aside> <!-- end sidebar -->
 
                 <div class="span6">
-                    <div class="row">
-                        @yield('content')
+                    <div class="post">
+                        <div class="row">
+                            @yield('content')
+                        </div>
                     </div>
                 </div>
 
