2107f6752aedf070cdd2ba993b10cc8e0540102c
Paiboon Auengkongkatong <paiboon15721@gmail.com>
2014-06-04 16:34:58 +0700

    fix new design

--- app/views/contentLayouts/main.blade.php
+++ app/views/contentLayouts/main.blade.php
@@ -22,7 +22,37 @@
 
 @section('content')
 <div class="span6">
-    @yield('subContent')
+    <div class="post">
+        <div class="row">
+            <div class="feat-img span2">
+                <img src="http://lorempixel.com/350/325/sports/1" title="featured image" alt="an image was here" />
+            </div><!-- end featured image -->
+
+            <div class="details span4">
+                <h2>ข่าวประชาสัมพันธ์ของศูนย์</h2>
+                <p>
+                    Lorem ipsum pariatur velit pariatur dolore aliqua voluptate sed Duis dolor ea cillum qui anim consequat quis sunt ex eu culpa veniam nisi nulla non dolor esse sit aliquip irure in amet reprehenderit quis voluptate occaecat in enim Duis.Aliqua voluptate sed Duis dolor ea cillum qui anim consequat quis sunt ex eu culpa veniam nisi nulla non dolor esse sit aliquip irure in amet reprehenderit quis voluptate occaecat in enim Duis.
+                </p>
+            </div> <!-- end details -->
+            <a class="btn pull-right" href="#">อ่านต่อ..</a>
+        </div><!-- end post row -->
+    </div><!-- end post -->
+
+    <div class="post">
+        <div class="row">
+            <div class="feat-img span2">
+                <img src="http://lorempixel.com/350/325/sports/3" title="featured image" alt="an image was here" />
+            </div><!-- end featured image -->
+
+            <div class="details span4">
+                <h2>ภาพกิจกรรมต่างๆ ของศูนย์</h2>
+                <p>
+                    Lorem ipsum pariatur velit pariatur dolore aliqua voluptate sed Duis dolor ea cillum qui anim consequat quis sunt ex eu culpa veniam nisi nulla non dolor esse sit aliquip irure in amet reprehenderit quis voluptate occaecat in enim Duis.Aliqua voluptate sed Duis dolor ea cillum qui anim consequat quis sunt ex eu culpa veniam nisi nulla non dolor esse sit aliquip irure in amet reprehenderit quis voluptate occaecat in enim Duis.
+                </p>
+            </div> <!-- end details -->
+            <a class="btn pull-right" href="#">อ่านต่อ..</a>
+        </div><!-- end post row -->
+    </div><!-- end post -->
 </div>
 @stop
 
@@ -44,11 +74,11 @@
 <aside class="span3">
     <div class="row">
         <div class="widget span3">
-            <h3>ข่าวประชาสัมพันธ์ของศูนย์</h3>
+            <h3>โครงการพระราชดำริ</h3>
             <p><img src="{{asset('images/main/472875.gif')}}" title="widget image" alt="an image was here" width="100%" height="100%" /></p>
         </div><!-- end widget -->
         <div class="widget span3">
-            <h3>ภาพกิจกรรมต่างๆ ของศูนย์</h3>
+            <h3>ปฏิทินข่าวกิจกรรมของศูนย์</h3>
             <p><img src="{{asset('images/main/calendar.jpg')}}" title="widget image" alt="an image was here" width="100%" height="100%" /></p>
         </div><!-- end widget -->
         <div class="widget span3">
