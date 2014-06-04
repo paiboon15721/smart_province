ec3786c1175b9373fa64972d0720c3556b553933
Paiboon Auengkongkatong <paiboon15721@gmail.com>
2014-06-04 14:53:14 +0700

    fix new design

--- app/views/content/main.blade.php
+++ app/views/content/main.blade.php
@@ -16,15 +16,14 @@
             <img src="http://lorempixel.com/350/325/sports/1" title="featured image" alt="an image was here" />
         </div><!-- end featured image -->
 
-        <div class="details span7">
-            <h2>Just a Nice Post!</h2>
+        <div class="details span5">
+            <h2>ข่าวประชาสัมพันธ์ของศูนย์</h2>
             <p>
                 Lorem ipsum pariatur velit pariatur dolore aliqua voluptate sed Duis dolor ea cillum qui anim consequat quis sunt ex eu culpa veniam nisi nulla non dolor esse sit aliquip irure in amet reprehenderit quis voluptate occaecat in enim Duis.Aliqua voluptate sed Duis dolor ea cillum qui anim consequat quis sunt ex eu culpa veniam nisi nulla non dolor esse sit aliquip irure in amet reprehenderit quis voluptate occaecat in enim Duis.
             </p>
         </div> <!-- end details -->
         <a class="btn pull-right" href="#">Read More!</a>
     </div><!-- end post row -->
-
 </div><!-- end post -->
 
 <div class="post">
@@ -33,31 +32,13 @@
             <img src="http://lorempixel.com/350/325/sports/3" title="featured image" alt="an image was here" />
         </div><!-- end featured image -->
 
-        <div class="details span7">
-            <h2>Just a Nice Post!</h2>
+        <div class="details span5">
+            <h2>ภาพกิจกรรมต่างๆ ของศูนย์</h2>
             <p>
                 Lorem ipsum pariatur velit pariatur dolore aliqua voluptate sed Duis dolor ea cillum qui anim consequat quis sunt ex eu culpa veniam nisi nulla non dolor esse sit aliquip irure in amet reprehenderit quis voluptate occaecat in enim Duis.Aliqua voluptate sed Duis dolor ea cillum qui anim consequat quis sunt ex eu culpa veniam nisi nulla non dolor esse sit aliquip irure in amet reprehenderit quis voluptate occaecat in enim Duis.
             </p>
         </div> <!-- end details -->
         <a class="btn pull-right" href="#">Read More!</a>
     </div><!-- end post row -->
-
-</div><!-- end post -->
-
-<div class="post">
-    <div class="row">
-        <div class="feat-img span2">
-            <img src="http://lorempixel.com/350/325/sports/2" title="featured image" alt="an image was here" />
-        </div><!-- end featured image -->
-
-        <div class="details span7">
-            <h2>Just a Nice Post!</h2>
-            <p>
-                Lorem ipsum pariatur velit pariatur dolore aliqua voluptate sed Duis dolor ea cillum qui anim consequat quis sunt ex eu culpa veniam nisi nulla non dolor esse sit aliquip irure in amet reprehenderit quis voluptate occaecat in enim Duis.Aliqua voluptate sed Duis dolor ea cillum qui anim consequat quis sunt ex eu culpa veniam nisi nulla non dolor esse sit aliquip irure in amet reprehenderit quis voluptate occaecat in enim Duis.
-            </p>
-        </div> <!-- end details -->
-        <a class="btn pull-right" href="#">Read More!</a>
-    </div><!-- end post row -->
-
 </div><!-- end post -->
 @stop
