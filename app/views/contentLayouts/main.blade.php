da83cdc43f3234a884d4b30915c2261b24c18807
Paiboon Auengkongkatong <paiboon15721@gmail.com>
2014-06-05 14:23:20 +0700

    fix new design

--- app/views/contentLayouts/main.blade.php
+++ app/views/contentLayouts/main.blade.php
@@ -85,7 +85,7 @@
         </div><!-- end widget -->
         <div class="widget span3">
             <h3>ปฏิทินข่าวกิจกรรมของศูนย์</h3>
-            <iframe src="https://www.google.com/calendar/embed?showTitle=0&amp;showPrint=0&amp;showTabs=0&amp;showTz=0&amp;height=600&amp;wkst=1&amp;hl=th&amp;bgcolor=%23FFFFFF&amp;ctz=Asia%2FBangkok" style=" border-width:0 " width="800" height="600" frameborder="0" scrolling="no"></iframe>
+            <iframe src="https://www.google.com/calendar/embed?showTitle=0&amp;showPrint=0&amp;showTabs=0&amp;showCalendars=0&amp;showTz=0&amp;height=600&amp;wkst=1&amp;hl=th&amp;bgcolor=%23FFFFFF&amp;ctz=Asia%2FBangkok" style=" border-width:0 " width="100%" height="100%" frameborder="0" scrolling="no"></iframe>
         </div><!-- end widget -->
         <div class="widget span3">
             <p><img src="{{asset('images/main/banner(2).jpg')}}" title="widget image" alt="an image was here" width="100%" height="100%" /></p>
