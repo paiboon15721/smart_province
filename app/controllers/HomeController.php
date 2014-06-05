3083018ec88a339eb3042bca12b6e4c49f952d9a
Paiboon Auengkongkatong <paiboon15721@gmail.com>
2014-06-05 09:10:53 +0700

    fix new design

--- app/controllers/HomeController.php
+++ app/controllers/HomeController.php
@@ -10,7 +10,7 @@ class HomeController extends BaseController {
     }
 
     public function writeSession($catm) {
-        $catm = Catm::select('catm_id', 'catm_name_th')->find($catm);
+        $catm = Catm::select('catm_id', 'catm_name_th', 'catm_name_en')->find($catm);
         Session::put('catmId', $catm->catm_id);
         Session::put('catmNameEn', $catm->catm_name_en);
         Session::put('catmNameTh', $catm->catm_name_th);
