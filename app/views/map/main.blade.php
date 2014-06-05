6bce352c1190a8fb0bc22f808c0a2f5431dd4239
Paiboon Auengkongkatong <paiboon15721@gmail.com>
2014-06-05 15:00:38 +0700

    fix new design

--- app/views/map/main.blade.php
+++ app/views/map/main.blade.php
@@ -12,13 +12,17 @@
 @section('subContent')
 <div class="post">
     <div class="row">
-        <div class="details span6">
-            <h2>เลือกหมู่บ้านที่ต้องการ</h2>
+        <div class="feat-img span2">
+            <img src="http://lorempixel.com/350/325/sports/1" title="featured image" alt="an image was here" />
+        </div><!-- end featured image -->
+
+        <div class="details span4">
+            <h2>ข่าวประชาสัมพันธ์ของศูนย์</h2>
             <p>
-                {{Form::select('catmId', $catmNameThList)}}
-                <a class="btn" href="#">ตกลง</a>
+                Lorem ipsum pariatur velit pariatur dolore aliqua voluptate sed Duis dolor ea cillum qui anim consequat quis sunt ex eu culpa veniam nisi nulla non dolor esse sit aliquip irure in amet reprehenderit quis voluptate occaecat in enim Duis.Aliqua voluptate sed Duis dolor ea cillum qui anim consequat quis sunt ex eu culpa veniam nisi nulla non dolor esse sit aliquip irure in amet reprehenderit quis voluptate occaecat in enim Duis.
             </p>
         </div> <!-- end details -->
+        <a class="btn pull-right" href="#">อ่านต่อ..</a>
     </div><!-- end post row -->
 </div><!-- end post -->
 
