75d2054a651e123ca01f30a0dd99d15c1a727863
Paiboon Auengkongkatong <paiboon15721@gmail.com>
2014-06-06 12:26:41 +0700

    fix new design

--- app/views/contentLayouts/base.blade.php
+++ app/views/contentLayouts/base.blade.php
@@ -74,7 +74,7 @@
                      onmouseover="this.src ='{{asset('images/main/card_selected.png')}}';"
                      onmouseout="this.src ='{{asset('images/main/card_unselected.png')}}';"/>
             </p>
-            {{HTML::link('main', 'หน้าหลัก', array('class' => 'btn pull-right'))}}
+            {{HTML::link('main', 'หน้าหลัก', array('class' => 'btn center'))}}
         </div><!-- end widget -->
         <div class="widget span3">
             <h3>ปฏิทินข่าวกิจกรรมของหมู่บ้าน</h3>
