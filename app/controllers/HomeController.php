85ce6c8607a638bfe8733feed35f9e3a0fb2a85b
Paiboon Auengkongkatong <paiboon15721@gmail.com>
2014-06-06 15:22:00 +0700

    fix new design

--- app/controllers/HomeController.php
+++ app/controllers/HomeController.php
@@ -12,6 +12,7 @@ class HomeController extends BaseController {
     public function logout() {
         Session::flush();
         session_destroy();
+        return Redirect::to('main');
     }
 
     public function bypassLogin() {
