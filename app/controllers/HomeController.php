ae8454068c2d1f0895bfc65be8084bf4979ed44e
Paiboon Auengkongkatong <paiboon15721@gmail.com>
2014-06-06 14:16:45 +0700

    fix new design

--- app/controllers/HomeController.php
+++ app/controllers/HomeController.php
@@ -33,7 +33,7 @@ class HomeController extends BaseController {
             $_SESSION['EXPIRE'] = $_SESSION['START'] + 1800;
             $_SESSION['catm_login'] = $emp->ccaattmm;
 
-            return redirecct::to('main');
+            return redirect::to('main');
         } else {
 
         }
