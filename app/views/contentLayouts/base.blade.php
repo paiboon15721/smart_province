aff553e08eb455620517c314c3df471552577371
Paiboon Auengkongkatong <paiboon15721@gmail.com>
2014-06-06 12:27:40 +0700

    fix new design

--- app/views/contentLayouts/base.blade.php
+++ app/views/contentLayouts/base.blade.php
@@ -25,10 +25,6 @@
             window.close();
             }, 2000);
     }
-
-    function bypassLogin() {
-    window.location = "bypassLogin";
-    }
 </script>
 @yield('subScript&css')
 @stop
@@ -74,7 +70,9 @@
                      onmouseover="this.src ='{{asset('images/main/card_selected.png')}}';"
                      onmouseout="this.src ='{{asset('images/main/card_unselected.png')}}';"/>
             </p>
-            {{HTML::link('main', 'หน้าหลัก', array('class' => 'btn'))}}
+            <p style="text-align:center;" >
+                {{HTML::link('bypassLogin', 'BYPASS', array('class' => 'btn'))}}
+            </p>
         </div><!-- end widget -->
         <div class="widget span3">
             <h3>ปฏิทินข่าวกิจกรรมของหมู่บ้าน</h3>
