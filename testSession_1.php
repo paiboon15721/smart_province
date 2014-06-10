6eba3407bf94f86e78ad87541d082f4197a85f91
Paiboon Auengkongkatong <paiboon15721@gmail.com>
2014-06-10 11:40:37 +0700

    fix bug

--- testSession_1.php
+++ testSession_1.php
@@ -1,5 +1,11 @@
 <?php
 
+session_start();
+
+echo "<h3> PHP List All Session Variables</h3>";
+foreach ($_SESSION as $key => $val)
+    echo $key . " " . $val . "<br/>";
+
 //session_start();
 //$_SESSION['EMPID'] = 'test';
 $_SESSION['EMPID'] = 'test';
