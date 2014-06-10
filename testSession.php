32eedd96c8df2b8f26731b8f02489c77cdad115e
Paiboon Auengkongkatong <paiboon15721@gmail.com>
2014-06-10 11:27:37 +0700

    fix bug

--- testSession.php
+++ testSession.php
@@ -1,7 +1,7 @@
 <?php
 
 session_save_path('/var/www/html/Nayok_laravel/cgi-bin/tmp');
-//session_start();
+session_start();
 //$_SESSION['EMPID'] = 'test';
 echo $_SESSION['EMPID'];
 echo $_SERVER['SCRIPT_FILENAME'];
