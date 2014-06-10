56ec90c52a5c04cb21506ca07d9ebaa902ee5531
Paiboon Auengkongkatong <paiboon15721@gmail.com>
2014-06-10 10:21:56 +0700

    fix bug

--- public/external_project/poll/mainmenu.php
+++ public/external_project/poll/mainmenu.php
@@ -1,6 +1,6 @@
 <?php
-header('Content-type: text/html; charset=utf-8');
 session_start();
+header('Content-type: text/html; charset=utf-8');
 require_once("inc/MySQL/mySQLFunc.php");
 require_once("inc/function.php");
 $_SESSION['EMPID'] = str_replace("-", "", $_SESSION['EMPID']);
