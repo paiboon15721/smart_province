6b703c50aa142d6c78853fee5d81b21025532b11
Paiboon Auengkongkatong <paiboon15721@gmail.com>
2014-06-11 09:50:58 +0700

    fix bug

--- app/controllers/HomeController.php
+++ app/controllers/HomeController.php
@@ -67,11 +67,11 @@ class HomeController extends BaseController {
     public function writeSession($catm) {
         $catm = Catm::select('catm_id', 'catm_name_th', 'catm_name_en')->find($catm);
 
-        Session::put('catmId', $catm->catm_id);
+        //Session::put('catmId', $catm->catm_id);
 
         Session::put('catmNameEn', $catm->catm_name_en);
 
-        Session::put('catmNameTh', $catm->catm_name_th);
+        //Session::put('catmNameTh', $catm->catm_name_th);
 
         $_SESSION['catm_menu'] = $catm->catm_id;
         $_SESSION['catm_description'] = $catm->catm_name_th;
