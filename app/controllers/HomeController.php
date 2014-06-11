2965e9a12af40b9e2962f7c14df01a0118be1911
Paiboon Auengkongkatong <paiboon15721@gmail.com>
2014-06-11 09:50:17 +0700

    fix bug

--- app/controllers/HomeController.php
+++ app/controllers/HomeController.php
@@ -10,14 +10,14 @@ class HomeController extends BaseController {
     }
 
     public function logout() {
-
-        Session::forget('EMPID');
-        Session::forget('EMPNAME');
-        Session::forget('EMPADD');
-        Session::forget('START');
-        Session::forget('EXPIRE');
-        Session::forget('catm_login');
-
+        /*
+          Session::forget('EMPID');
+          Session::forget('EMPNAME');
+          Session::forget('EMPADD');
+          Session::forget('START');
+          Session::forget('EXPIRE');
+          Session::forget('catm_login');
+         */
         unset($_SESSION['EMPID']);
         unset($_SESSION['EMPNAME']);
         unset($_SESSION['EMPADD']);
@@ -43,14 +43,15 @@ class HomeController extends BaseController {
     public function write_session($empId, $fName, $address) {
         $emp = Emp::find($empId);
         if ($emp->exists()) {
-
-            Session::put('EMPID', $empId);
-            Session::put('EMPNAME', rawurldecode($fName));
-            Session::put('EMPADD', rawurldecode($address));
-            Session::put('START', time());
-            Session::put('EXPIRE', time() + 1800);
-            Session::put('catm_login', $emp->ccaattmm);
-
+            /*
+              Session::put('EMPID', $empId);
+              Session::put('EMPNAME', rawurldecode($fName));
+              Session::put('EMPADD', rawurldecode($address));
+              Session::put('START', time());
+              Session::put('EXPIRE', time() + 1800);
+              Session::put('catm_login', $emp->ccaattmm);
+             *
+             */
             $_SESSION['EMPID'] = $empId;
             $_SESSION['EMPNAME'] = rawurldecode($fName);
             $_SESSION['EMPADD'] = rawurldecode($address);
@@ -65,9 +66,13 @@ class HomeController extends BaseController {
 
     public function writeSession($catm) {
         $catm = Catm::select('catm_id', 'catm_name_th', 'catm_name_en')->find($catm);
-        //Session::put('catmId', $catm->catm_id);
+
+        Session::put('catmId', $catm->catm_id);
+
         Session::put('catmNameEn', $catm->catm_name_en);
-        //Session::put('catmNameTh', $catm->catm_name_th);
+
+        Session::put('catmNameTh', $catm->catm_name_th);
+
         $_SESSION['catm_menu'] = $catm->catm_id;
         $_SESSION['catm_description'] = $catm->catm_name_th;
         return Redirect::to('main');
