ebf72410b6b6bc4bfa8ac97a3a84c9dec0c28a22
Paiboon Auengkongkatong <paiboon15721@gmail.com>
2014-06-12 15:47:13 +0700

    fix bug

--- app/views/imageSlideSetting/index.blade.php
+++ app/views/imageSlideSetting/index.blade.php
@@ -33,7 +33,7 @@
         <tr>
             <td style="text-align:center;">{{($seq++)}}</td>
             <td style="text-align:center;"><img src="{{asset('data/'. $data->image)}}" height="350" width="1200" /></td>
-            <td style="text-align:center;">{{HTML::link($url . '/update/' . $data->image_id, 'แก้ไข', array('class' => 'btn', 'style'=>'width:100'))}}<br /><br />{{HTML::link($url . '/delete/' . $data->image_id, 'ลบ', array('class' => 'btn'))}}</td>
+            <td style="text-align:center;">{{HTML::link($url . '/update/' . $data->image_id, 'แก้ไข', array('class' => 'btn', 'style'=>'width:100px'))}}<br /><br />{{HTML::link($url . '/delete/' . $data->image_id, 'ลบ', array('class' => 'btn'))}}</td>
         </tr>
         @endforeach
     </tbody>
